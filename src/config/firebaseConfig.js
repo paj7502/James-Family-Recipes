5038241625e45d4ee32df0b4770d4f3184cf8e1f
paj7502 <pauljames2002@hotmail.com>
2026-04-08 17:54:00 +0100

    Initialize Firebase configuration with environment variables

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the Firebase modules that you need in your app
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
